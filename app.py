1ea26c7e62a8edfdc29f7227fa12a939cbb18c5a
maury <maury@mauweezy.mauryquijada.me>
2014-04-18 04:46:02 -0400

    Initial commit.

---
+++ app.py
@@ -0,0 +1,184 @@
+import time
+import requests
+from flask import Flask, render_template, request, make_response
+import json
+import sqlite3
+import re
+from flask_errormail import mail_on_500
+from random import randint
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+app = Flask(__name__)
+app.config.from_pyfile('app.cfg', silent=True)
+
+FROM_EMAIL = app.config["FROM_EMAIL"]
+TO_EMAIL = app.config["TO_EMAIL"]
+SENDING_PHONE_NUMBER = app.config["SENDING_PHONE_NUMBER"]
+API_KEY = app.config["API_KEY"]
+API_SECRET = app.config["API_SECRET"]
+ADMINISTRATORS = app.config["ADMINISTRATORS"]
+BASE_URL = "https://rest.nexmo.com/sms/json?api_key={0}\
+&api_secret={1}&type={2}&from={3}&to={4}&text={5}"
+
+mail_on_500(app, ADMINISTRATORS)
+
+def create_database():
+	conn = sqlite3.connect("records.db")
+	c = conn.cursor()
+	c.execute("CREATE TABLE records (added real, mobile_number real, course_id text, confirmed integer, keycode integer)")
+	conn.commit()
+	conn.close()
+
+@app.route("/")
+def hello():
+	# Simply display the homepage.
+	return render_template('index.html')
+
+@app.route("/receiveMessage", methods=['GET', 'POST'])
+def handle_message():
+	# Get the text message information.
+	message = request.form["text"]
+	phone = request.form["msisdn"]
+
+	# Prepare the database.
+	conn = sqlite3.connect("records.db")
+	c = conn.cursor()
+
+	if message[0:3] == "YES":
+		# Grab the keycode.
+		match = re.search("^YES (.*)$", message)
+		keycode = match.group(1)
+
+		# Set them to "confirmed"
+		c.execute("UPDATE records SET confirmed = 1, added = ? WHERE mobile_number = ? AND keycode = ? AND confirmed = 0", \
+			(str(int(time.time())), phone, keycode))
+
+		# Ensure that the update was successful. If not, e-mail the admin and let the user know.
+		if c.rowcount < 1:
+			log_error("Confirmation unsuccessful with {0} and {1}".format(phone, keycode))
+			msg_response = "Sorry, but 5C Enrollify had trouble processing your request."
+		else:
+			# Grab the course_id to use it in the response.
+			c.execute("SELECT course_id FROM records WHERE mobile_number = ? AND keycode = ?", (phone, keycode))
+			result = c.fetchone()
+			course_id = result[0]
+
+			msg_response = "Great! You'll now receive updates about {0}.".format(course_id)
+
+	elif message[0:4] == "STOP":
+		# Grab the keycode.
+		match = re.search("^STOP (.*)$", message)
+		keycode = match.group(1)
+
+		# Delete them!
+		c.execute("DELETE FROM records WHERE confirmed = 1 AND mobile_number = ? AND keycode = ?", \
+			(phone, keycode))
+
+		# Ensure that the update was successful. If not, e-mail the admin and let the user know.
+		if c.rowcount < 1:
+			log_error("Unsubscription unsuccessful with {0} and {1}".format(phone, keycode))
+			msg_response = "Sorry, but 5C Enrollify had trouble processing your request."
+		else:
+			msg_response = "You'll no longer receive notifications about that class."
+	else:
+		# Tell them that I didn't understand their request!
+		msg_response = "Sorry, but 5C Enrollify doesn't understand your input."
+
+	# Send the response.
+	send_message(phone, msg_response)
+
+	# Close the database connection.
+	conn.commit()
+	conn.close()
+
+	return "success", 200
+
+@app.route("/addRecord", methods=['POST'])
+def add_record_to_database():
+	# Create the response object.
+	response = make_response()
+
+	# Grab the input from the asynchronous request.
+	match = re.search("^([A-Z]+[0-9]+\s[A-Z]{2}-[0-9]{2}):.*$", str(request.form["course_id"]))
+	course_id = match.group(1)
+	phone = str("1" + request.form["phone"])
+
+	# Create a keycode that'll be used to add/ remove the course listing.
+	keycode = randint(10000, 99999)
+
+	# Insert it into the database.
+	conn = sqlite3.connect("records.db")
+	c = conn.cursor()
+
+	# Lazy check to see if the insertion succeeds.
+	try:
+		c.execute("INSERT INTO records VALUES (?, ?, ?, ?, ?)", (str(int(time.time())), phone, course_id, 0, keycode))
+		conn.commit()
+		conn.close()
+	except Exception as exception:
+		log_error("Addition unsuccessful: %s" % exception)
+		response.status_code = 500
+		return response
+
+	# Send the user a confirmation message.
+	message = "5C Enrollify received a request for this number to\
+	receive updates about {0}. Reply with \"YES {1}\" to confirm this.".format(course_id, keycode)
+	send_message(phone, message)
+
+	# If all is good, return.
+	response.status_code = 201
+	return response
+
+@app.route("/getCourses")
+def get_current_courses():
+	# Open the course information file.
+	f = open('depts_courses.json', 'r')
+	depts_info = json.loads(f.read())
+
+	# Prepare the the output. FIXME: Store this in memory.
+	course_strings = []
+	for dept in depts_info:
+		for course in dept["courses"]:
+			title = course["course"] + " " + course["campus"] + "-" + course["section"] + ": " + course["title"]
+			course_strings.append(title)
+
+	f.close()
+
+	return json.dumps(course_strings)
+
+# Sends the message to a receiver using the Nexmo API.
+def send_message(receiver, text):
+	type = "text"
+
+	request = BASE_URL.format(API_KEY, API_SECRET, type, SENDING_PHONE_NUMBER, receiver, text)
+	response = requests.get(request)
+	data = json.loads(response.text)
+
+	# If sending fails, send a message to the administrator.
+	if data["messages"][0]["status"] != "0":
+		body = "Received an error while trying to send a message: {0}".format(response.text)
+		log_error(body)
+		return False
+	else:
+		return True
+
+# Sends a message to the e-mail defined above.
+def log_error(body):
+	smtp = smtplib.SMTP('localhost')
+	msg = MIMEMultipart()
+	msg["Subject"] = "5C Enrollify Error"
+	msg["From"] = FROM_EMAIL
+	msg["To"] = TO_EMAIL[0]
+	msg.attach(MIMEText(body, "plain"))
+
+	smtp.helo()
+	smtp.sendmail(FROM_EMAIL, TO_EMAIL, msg.as_string())
+	smtp.close()
+
+	return True
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ portal_scraper.py
@@ -0,0 +1,222 @@
+import sqlite3
+import urllib2
+import requests
+import re
+from bs4 import BeautifulSoup
+import json
+import os.path
+from app import send_message
+
+# Set up globals.
+BASEURL = "https://portal.claremontmckenna.edu/ics/Portlets/CRM/CXWebLinks/Port\
+let.CXFacultyAdvisor/CXFacultyAdvisorPage.aspx?SessionID={25715df1-32b9-42bf-90\
+33-e5630cfbf34a}&MY_SCP_NAME=/cgi-bin/course/pccrscatarea.cgi&DestURL=http://cx\
+-cmc.cx.claremont.edu:51081/cgi-bin/course/pccrslistarea.cgi?crsarea=%s&yr=2014\
+&sess=FA"
+
+# Given a department, it creates a list of dictionaries, each of which contains
+# course information for a course in that department.
+def grab_course_info_for_dept(dept_id):
+	r = requests.get(BASEURL % dept_id)
+	soup = BeautifulSoup(r.text)
+	table = soup.find_all("table")[-1]  # last table is `All Sections"
+	department_rows = table.find_all("tr", class_="glb_data_dark")
+
+	course_list = []
+
+	for i, t in enumerate(department_rows):
+		course = create_course_dict(get_td_tags(t))
+		
+		if course and course not in course_list:
+			course_list.append(course)
+
+	return course_list
+
+# Given two enrollment lists for the same department, it creates a list
+# of dictionaries, each of which contains the course code that changed
+# with old and new enrollment statistics.
+def find_enrollment_delta_for_dept(old_enrollment, new_enrollment):
+	deltas = []
+	for old_course in old_enrollment:
+		for new_course in new_enrollment:
+			if old_course["course"] == new_course["course"] \
+				and old_course["section"] == new_course["section"] \
+				and old_course["campus"] == new_course["campus"]:
+
+				if not (old_course["enrolled"] == new_course["enrolled"] and old_course["max"] == new_course["max"]):
+					course_name = old_course["course"] + " " + old_course["campus"] + "-" + old_course["section"]
+					deltas.append({"course": course_name, "old_enrolled": old_course["enrolled"], "new_enrolled": new_course["enrolled"], \
+							"old_max": old_course["max"], "new_max": new_course["max"]})
+
+	return deltas
+
+# Takes a <tr> tag as input and returns all the <td> tags
+# contained inside. Strips all whitespace and the `Textbook Info"
+# string after the course title.
+def get_td_tags(tr_tag):
+	tds = [str(td.text.strip()) for td in tr_tag.find_all("td")]
+	tds[-1] = remove_spaces(tds[-1])
+	return tds
+
+# Returns a string that's truncated after two double spaces (as long as it's not
+# seen in the first three characters).
+def remove_spaces(string):
+	if string.find("  ") < 3:
+		return string[:string.find("  ", 3)]
+	else:
+		return string[:string.find("  ")]
+
+# Creates a dictionary representing a course given a row.
+def create_course_dict(td_tags):
+	length = len(td_tags)
+	course = {}
+
+	# These are the only row lengths that show full sections.
+	if length == 12 or length == 14:
+		course["course"] = remove_spaces(td_tags[0]).replace(" ", "")
+		course["section"] = td_tags[1]
+		course["campus"] = td_tags[7]
+
+		reg_limit = td_tags[3]
+		match = re.search("^\s*([0-9]+)\s+/\s+([0-9]+)\s*$", reg_limit)
+		course["enrolled"] = match.group(1)
+		course["max"] = match.group(2)
+
+		# Skip PE079 for now.
+		if course["course"] is "PE079":
+			return False
+		else:
+			if length == 12:
+				course["title"] = td_tags[11]
+				return course
+
+			else:
+				course["title"] = td_tags[13]
+				return course
+
+	elif length in [6, 7, 8]:
+		return False
+	else:
+		print "oops - %d inner <td> tags at index %d" % (length, index)
+		return False
+
+def construct_depts():
+	# Only construct if we don"t have it.
+	if os.path.isfile("depts.json"):
+		print "Skipping constructing depts.json; we already have it."
+		return False
+
+	f = open("depts.json", "w")
+
+	# Grab all of the available courses and save to a JSON file.
+	print "Fetching all departments..."
+	request = urllib2.urlopen("http://course-api.herokuapp.com/")
+	data = json.load(request)
+	depts = []
+
+	for dept in data:
+		depts.append(dept.keys()[0][1:])
+
+	f.write(json.dumps(depts, indent=4, sort_keys=True))
+	f.close()
+
+	return True
+
+def construct_course_info():
+	# Only construct if we don"t have it.
+	if os.path.isfile("depts_courses.json"):
+		print "Skipping constructing depts_courses.json; we already have it."
+		return False
+
+	# Construct depts.json.
+	construct_depts()
+
+	# Open the file containing all of the departments.
+	f = open("depts.json", "r")
+	depts = json.loads(f.read())
+	f.close()
+
+	# Open a new depts_courses.json file.
+	f = open("depts_courses.json", "w")
+	dept_list = []
+
+	# Write all department and course information into that file.
+	for dept in depts:
+		print "Grabbing course information for {0}...".format(dept)
+		course_info = grab_course_info_for_dept(dept)
+		dept_list.append({"dept": dept, "courses": course_info})
+
+	f.write(json.dumps(dept_list, indent=4, sort_keys=True))
+	f.close()
+
+	return True
+
+def update_course_info():
+	# Construct the course information file only if necessary.
+	construct_course_info()
+
+	# Open the file containing all of the departments.
+	f = open("depts.json", "r")
+	depts = json.loads(f.read())
+	f.close()
+
+	# Open a new course information file.
+	g = open("depts_courses.json", "r")
+	depts_info = json.loads(g.read())
+	g.close()
+	new_depts_info = []
+
+	# Refer to all course objects that show a change as deltas.
+	deltas = []
+
+	# Update course information.
+	for dept in depts:
+		print "Updating course information for {0}...".format(dept)
+		new_dept_info = grab_course_info_for_dept(dept)
+		new_depts_info.append({"dept": dept, "courses": new_dept_info})
+
+		# Find the courses for the department we"re looking at.
+		for saved_dept in depts_info:
+			if saved_dept["dept"] == dept:
+				old_dept_info = saved_dept["courses"]
+				break
+
+		# Find the enrollment numbers that changed.
+		new_deltas = find_enrollment_delta_for_dept(old_dept_info, new_dept_info)
+
+		# Take care of cross-listed courses. Do not send updates more than once.
+		for new_delta in new_deltas:
+			if new_delta not in deltas:
+				deltas.append(new_delta)
+
+		print new_deltas
+
+	g = open("depts_courses.json", "w")
+	g.write(json.dumps(new_depts_info, indent=4, sort_keys=True))
+	g.close()
+
+	return deltas
+
+def send_updates():
+	# Update the course information and obtain the deltas.
+	deltas = update_course_info()
+
+	# Open the database connection.
+	conn = sqlite3.connect("records.db")
+	c = conn.cursor()
+
+	# For each changed course number, query the database and send messages to
+	# those subscribed to that course.
+	changed_courses = []
+	for delta in deltas:
+		c.execute("SELECT mobile_number, keycode FROM records WHERE confirmed = 1 AND course_id = ?", (delta["course"],))
+		numbers = c.fetchall()
+		for number in numbers:
+			send_message(str(int(number[0])), "As of now, {0} / {1} seats are now taken in {4} (previously {2} / {3}).\
+			Text \"STOP {5}\" to stop receiving updates.".format(delta["new_enrolled"], delta["new_max"],\
+			delta["old_enrolled"], delta["old_max"], delta["course"], str(int(number[1]))))
+
+	return True
+
+if __name__ == "__main__":
+    send_updates()
